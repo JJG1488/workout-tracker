7e01e7ce63b6e93c535a5766dc31ddfe282ee1c0
James Gault <jamesgault1488@gmail.com>
2021-10-26 04:09:37 -0400

    updating exercise file route

--- docs/routes/htmlRoutes.js
+++ docs/routes/htmlRoutes.js
@@ -18,7 +18,7 @@ module.exports = function(app) {
   });
 
   app.get("/exercise", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/exercise.html"))
+    res.sendFile(path.join(__dirname, "../../public/exercise.html"))
   });
 
   app.get("/stats", function(req, res) {
