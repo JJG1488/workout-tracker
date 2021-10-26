26bdf1f584fd87a87e3ce49874c6df77089fe45e
James Gault <jamesgault1488@gmail.com>
2021-10-26 04:02:09 -0400

    configuring routes for docs folder

--- docs/routes/htmlRoutes.js
+++ docs/routes/htmlRoutes.js
@@ -1,15 +1,27 @@
 const path = require("path");
 
 module.exports = function(app) {
+  // app.get("/", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/index.html"));
+  // });
+
+  // app.get("/exercise", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/exercise.html"))
+  // });
+
+  // app.get("/stats", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/stats.html"))
+  // });
+
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+    res.sendFile(path.join(__dirname, "../index.html"));
   });
 
   app.get("/exercise", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/exercise.html"))
+    res.sendFile(path.join(__dirname, "../exercise.html"))
   });
 
   app.get("/stats", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/stats.html"))
+    res.sendFile(path.join(__dirname, "../stats.html"))
   });
 };
