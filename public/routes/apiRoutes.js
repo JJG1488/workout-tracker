11c6b5729b988ff04ef86ce7605bd548bdf1e9b5
James Gault <jamesgault1488@gmail.com>
2021-11-05 18:22:39 -0400

    added routes to routes folder

---
+++ public/routes/apiRoutes.js
@@ -0,0 +1,74 @@
+const db = require("../models");
+
+module.exports = function (app) {
+    app.get("/api/workouts", function (req, res) {
+        db.Workout.find({}).then(function (dbWorkouts) {
+            res.json(dbWorkouts);
+        })
+        // Workout.aggregate([
+        //     {
+        //         $addFields: {
+        //             totalDuration: { $sum: "$exercises.duration" }
+        //         }
+        //     }
+        // ])
+        //     .sort({ date: -1 })
+        //     .then(dbWorkout => {
+        //         res.json(dbWorkout);
+        //     })
+        //     .catch(err => {
+        //         res.status(400).json(err);
+        //     })
+            .catch(err => { res.json(err) });
+    });
+
+    app.post("/api/workouts", function ({ body }, res) {
+        db.Workout.create(body)
+            .then(dbworkout => res.json(dbworkout))
+            .catch(err => res.json(err));
+    })
+
+    app.put("/api/workouts/:id", function (req, res) {
+        console.log(req.body);
+        db.Workout.findByIdAndUpdate(req.params.id, {
+            $push: {
+                exercises: req.body
+            }
+        }).then(dbWorkout => {
+            res.json(dbWorkout);
+        }).catch(err => res.json(err));
+
+
+    });
+
+    app.get("/api/workouts/range", (req, res) => {
+        db.Workout.find({}).then(dbUser => {
+            res.json(dbUser);
+        })
+        // Workout.aggregate([
+        //     {
+        //         $addFields: {
+        //             totalDuration: { $sum: "$exercises.duration" }
+        //         }
+        //     }
+        // ])
+        //     .sort({ date: -1 })
+        //     .then(dbWorkout => {
+        //         res.json(dbWorkout);
+        //     })
+        //     .catch(err => {
+        //         res.status(400).json(err);
+        //     })
+            .catch(err => res.json(err));
+    })
+
+    app.post("/api/workouts/range", function ({ body }, res) {
+        db.Workout.create(body).then(dbUser => {
+            res.json(dbUser);
+        })
+            .catch(err => res.json(err));
+    })
+
+};
+
+
